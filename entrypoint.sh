523e054167d77dfc835460062814b42ebf624356
KBrand <kbrandn25@gmail.com>
2026-08-13 16:57:24 +0100

    feat: enhance entrypoint script to report total monitors, successes, and failures

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,31 @@ response=$(curl -s "${API_URL}" )
 
 
 echo "$response" | jq '.' > data.json
-echo "Project is working"
-# https://pulse-api-xrla.onrender.com/monitors/2/checks&limit=30
 
+
+report=$(jq -r '.checks[] | "\(.id) \(.ok) \(.status_code)"' data.json |
+awk '
+{
+    total++
+
+    if ($2 == "true" && $3 == 200)
+        success++
+    else
+        failed++
+}
+END {
+    printf "%d %d %d", total, success, failed
+}')
+
+read -r total success failed <<< "$report"
+
+echo "Total Monitors: $total"
+echo "🚨 ALERT: $failed"
+echo "Sucess: $success"
+
+
+if [ -n "$WEBHOOK_URL" ]; then
+    curl -s -x POST "${WEBHOOK_URL}" \
+    -H 'Content-Type: application/json' \
+    -d "{\"text\": \"Project is working\", \"Total Monitors\": \"${total}\", \"🚨 ALERT: \": \"${failed}\", \" Sucess\": \"${success}\"}"
+fi 
