dad1c9ac97347e1dfb252ef5ba8106fbb19743fa
KBrand <kbrandn25@gmail.com>
2026-08-13 12:08:07 +0100

    feat: update entrypoint script and docker configuration for environment handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,18 @@
 
 set -euo pipefail
 
-source ./env.sh
+if [ -f "./env.sh" ]; then
+    source ./env.sh
+fi
 
 if [ -z "$WEBHOOK_URL" ]; then 
   echo "Error: WEBHOOK_URL is not set. Run 'source env.sh' first."
   exit 1
 fi
 
-echo " WEBHOOK_URL: $WEBHOOK_URL"
-# https://pulse-api-xrla.onrender.com/
+response=$(curl -s "https://pulse-api-xrla.onrender.com/monitors/2/checks?limit=30" | jq -r)
+
+
+echo "$response" | jq '.' > ./data.json
+# https://pulse-api-xrla.onrender.com/monitors/2/checks&limit=30
 
